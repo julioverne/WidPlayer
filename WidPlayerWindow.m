0cb7252ebdcc8e32cb18e91b1c68b5b47569462b
julioverne <jlio_verne@hotmail.com>
2016-03-21 15:34:29 -0300

    update to source-code v2.7
    update to source-code v2.7

--- WidPlayerWindow.m
+++ WidPlayerWindow.m
@@ -268,35 +268,4 @@
 	[self addGestureRecognizer:self.panGesture];
 }
 
-/*- (BOOL)shouldAffectStatusBarAppearance
-{
-    return [self isKeyWindow];
-}
-
-- (BOOL)canBecomeKeyWindow
-{
-    return [self.eventDelegate canBecomeKeyWindow];
-}
-
-+ (void)initialize
-{
-    // This adds a method (superclass override) at runtime which gives us the status bar behavior we want.
-    // The FLEX window is intended to be an overlay that generally doesn't affect the app underneath.
-    // Most of the time, we want the app's main window(s) to be in control of status bar behavior.
-    // Done at runtime with an obfuscated selector because it is private API. But you shoudn't ship this to the App Store anyways...
-	
-    NSString *canAffectSelectorString = [@[@"_can", @"Affect", @"Status", @"Bar", @"Appearance"] componentsJoinedByString:@""];
-    SEL canAffectSelector = NSSelectorFromString(canAffectSelectorString);
-    Method shouldAffectMethod = class_getInstanceMethod(self, @selector(shouldAffectStatusBarAppearance));
-    IMP canAffectImplementation = method_getImplementation(shouldAffectMethod);
-    class_addMethod(self, canAffectSelector, canAffectImplementation, method_getTypeEncoding(shouldAffectMethod));
-
-    // One more...
-    NSString *canBecomeKeySelectorString = [NSString stringWithFormat:@"_%@", NSStringFromSelector(@selector(canBecomeKeyWindow))];
-    SEL canBecomeKeySelector = NSSelectorFromString(canBecomeKeySelectorString);
-    Method canBecomeKeyMethod = class_getInstanceMethod(self, @selector(canBecomeKeyWindow));
-    IMP canBecomeKeyImplementation = method_getImplementation(canBecomeKeyMethod);
-    class_addMethod(self, canBecomeKeySelector, canBecomeKeyImplementation, method_getTypeEncoding(canBecomeKeyMethod));
-}*/
-
 @end
